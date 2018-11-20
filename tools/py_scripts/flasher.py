ec032a62425182b56c6cafe3d17ea939c490ac84
cmheia <cmheia@gmail.com>
2018-11-21 01:13:29 +0800

    串口烧片工具增加波特率设定

--- tools/py_scripts/flasher.py
+++ tools/py_scripts/flasher.py
@@ -14,6 +14,7 @@ import serial.tools.list_ports as list_ports
 
 from xmodem import XMODEM1k
 
+DEFAULT_BAUD = 2000000
 INIT_BAUD = 115200
 INIT_TIMEOUT = 1
 
@@ -24,6 +25,13 @@ BAUD_SET_CMD = {
     460800: bytes.fromhex('210a0007003100000000080700'),
     115200: bytes.fromhex('210a00974b3100000000c20100'),
 }
+
+
+def valid_speed(baud=DEFAULT_BAUD):
+    if baud in BAUD_SET_CMD:
+        return True
+    else:
+        return False
 
 
 def is_serial_avaliable(port):
@@ -55,12 +63,13 @@ class config_helper(object):
 
 
 class w600dl(object):
-    def __init__(self, image='', port=''):
+    def __init__(self, image='', port='', baud=0):
+        self._log = logging.getLogger(self.__class__.__name__)
+        logging.basicConfig(level=logging.INFO)
+        self._image = image
         if os.path.isfile(image):
-            self._log = logging.getLogger(self.__class__.__name__)
-            logging.basicConfig(level=logging.INFO)
+            save_conf = False
 
-            self._image = image
             self._total_packets = int(
                 (os.stat(self._image).st_size + 1023) / 1024)
 
@@ -71,25 +80,46 @@ class w600dl(object):
             self._config = self._cfg.load()
             if not 'base' in self._config:
                 self._config['base'] = {}
+                save_conf = True
 
             if is_serial_avaliable(port):
-                self._config['base']['port'] = port
+                if (not 'port' in self._config['base']) or self._config['base']['port'] != port:
+                    self._config['base']['port'] = port
+                    save_conf = True
             else:
-                if port in self._config['base']:
+                if 'port' in self._config['base']:
                     port = self._config['base']['port']
+                    self._log.warning('conf port is {}'.format(port))
+                else:
+                    self._config['base']['port'] = port
+                    save_conf = True
 
             if not is_serial_avaliable(port):
-                raise NameError('port {} NOT exist'.format(port))
+                raise RuntimeError('port {} NOT exist'.format(port))
+
+            if not valid_speed(baud):
+                if 'baud' in self._config['base'] and valid_speed(int(self._config['base']['baud'])):
+                    baud = int(self._config['base']['baud'])
+                    self._log.warning('conf baud is {}'.format(baud))
+                else:
+                    baud = DEFAULT_BAUD
+                    self._log.warning('default baud is {}'.format(baud))
+                    self._config['base']['baud'] = baud
+                    save_conf = True
+
+            self._port = port
+            self._baud = baud
 
             self._log.warning(
-                'open {} with {}, {}'.format(port, INIT_BAUD, INIT_TIMEOUT))
+                'comm {} with {}, {}'.format(port, self._baud, INIT_TIMEOUT))
             self._ser = serial.Serial(
                 port=port, baudrate=INIT_BAUD, timeout=INIT_TIMEOUT)
 
-            self._config['base']['port'] = port
-            self._cfg.save()
+            if save_conf:
+                self._cfg.save()
         else:
-            self._image = None
+            self._port = port
+            self._baud = 0
 
     def set_baudrate(self, baud):
         self._ser.baudrate = baud
@@ -126,8 +156,8 @@ class w600dl(object):
         self.send_cmd('get_loader_version')
         return self.readline()
 
-    def set_speed(self, baud=2000000):
-        if baud in BAUD_SET_CMD:
+    def set_speed(self, baud=DEFAULT_BAUD):
+        if valid_speed(baud):
             self._log.warning('set speed to {}'.format(baud))
             self.write(BAUD_SET_CMD[baud])
             return True
@@ -147,7 +177,7 @@ class w600dl(object):
         self._ser.close()
 
     def info(self):
-        return (self._ser.port, self._image)
+        return (self._port, self._image)
 
     def reset_device(self):
         self._log.warning('reset device')
@@ -156,7 +186,7 @@ class w600dl(object):
         self.set_reset(0)
         self.set_timeout(0.2)
 
-    def _check_baud(self, baud=2000000, retry=16):
+    def _check_baud(self, baud=DEFAULT_BAUD, retry=16):
         self._log.warning('check baud')
         checked = False
         while 0 < retry:
@@ -182,8 +212,10 @@ class w600dl(object):
             self._log.error('baud check failed')
         return checked
 
-    def sync_to_download(self, baud=2000000, retry=16 + len('secboot running V3.1...')):
+    def sync_to_download(self, retry=16 + len('secboot running V3.1...')):
         self._log.warning('sync to download')
+        if self._baud is 0:
+            raise RuntimeError('Image file NOT exist')
         state = 0
         synced = False
         c_count = 0
@@ -211,11 +243,11 @@ class w600dl(object):
             elif state is 2:  # PPP
                 self.enter_load_mode()
             elif state is 3:  # change baud
-                if self.set_speed(baud) is True:
-                    self._log.warning('change baud to {}'.format(baud))
+                if self.set_speed(self._baud) is True:
+                    self._log.warning('change baud to {}'.format(self._baud))
                     time.sleep(0.5)
-                    self.set_baudrate(baud)
-                    # synced = self._check_baud(baud, retry)
+                    self.set_baudrate(self._baud)
+                    # synced = self._check_baud(self._baud, retry)
                 synced = True
             c_retry -= 1
         if not synced:
@@ -260,7 +292,9 @@ def main(argv):
     argc = len(argv)
     print(argv)
     print(argc)
-    if argc == 3:
+    if argc == 4:
+        dl = w600dl(image=argv[1], port=argv[2], baud=int(argv[3]))
+    elif argc == 3:
         dl = w600dl(image=argv[1], port=argv[2])
     elif argc == 2:
         dl = w600dl(image=argv[1])
